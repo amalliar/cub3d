882cd59524c67b1053d87dd8495da662cb1259fd
Alexander Chetverkin <virtual-ins@mail.ru>
2020-08-03 00:18:59 +0300

    Refactor structs to access member structs through pointers.

--- src/wasd_circle.c
+++ src/wasd_circle.c
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/31 21:12:41 by amalliar          #+#    #+#             */
-/*   Updated: 2020/08/01 17:07:17 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/08/03 00:18:40 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,85 +18,94 @@
 
 typedef struct	s_circle
 {
-	int			x_pos;
-	int			y_pos;
+	int			xm;
+	int			ym;
 	int			radius;
 	int			color;
 }				t_circle;
 
-typedef struct	s_game_state
+typedef struct	s_data
 {
-	t_frame		frame;
-	t_circle	circle;
-}				t_game_state;
+	t_frame		*frame;
+	t_circle	*circle;
+}				t_data;
 
-int		keypress_handler(int keycode, t_game_state *game)
+int		keypress_handler(int keycode, t_data *data)
 {
 	switch (keycode)
 	{
 		case kVK_ANSI_W:
-			if ((game->circle).y_pos - (game->circle).radius > 1)
-				(game->circle).y_pos -= 1;
+			if (data->circle->ym - data->circle->radius > 1)
+				data->circle->ym -= 1;
 			break ;
 		case kVK_ANSI_A:
-			if ((game->circle).x_pos - (game->circle).radius > 1)
-				(game->circle).x_pos -= 1;
+			if (data->circle->xm - data->circle->radius > 1)
+				data->circle->xm -= 1;
 			break ;
 		case kVK_ANSI_S:
-			if ((game->circle).y_pos + (game->circle).radius < (game->frame).img_height - 1)
-				(game->circle).y_pos += 1;
+			if (data->circle->ym + data->circle->radius < data->frame->img_height - 1)
+				data->circle->ym += 1;
 			break ;
 		case kVK_ANSI_D:
-			if ((game->circle).x_pos + (game->circle).radius < (game->frame).img_width - 1)
-				(game->circle).x_pos += 1;
+			if (data->circle->xm + data->circle->radius < data->frame->img_width - 1)
+				data->circle->xm += 1;
 			break ;
 	}
-	// shift_color(game);
+	// TODO: Add color shifting depending on circle position within the image.
 	return (0);
 }
 
-int		render_next_frame(t_game_state *game)
+static void		drawcircle_f(t_frame *frame, int xm, int ym, int r, int clr)
 {
-
-	int		r;
-
-	(game->frame).img = mlx_new_image((game->frame).mlx, (game->frame).img_width, \
-		(game->frame).img_height);
-	(game->frame).addr = mlx_get_data_addr((game->frame).img, &(game->frame).bits_per_pixel, \
-		&(game->frame).line_length, &(game->frame).endian);
-	r = (game->circle).radius;
 	while (r >= 0)
 	{
-		drawcircle(&game->frame, (game->circle).x_pos, (game->circle).y_pos, \
-			r, (game->circle).color);
+		drawcircle(frame, xm, ym, r, clr);
 		--r;
 	}
-	mlx_put_image_to_window((game->frame).mlx, (game->frame).win, (game->frame).img, 0, 0);
-	mlx_destroy_image((game->frame).mlx, (game->frame).img);
+}
+
+static int		render_next_frame(t_data *data)
+{
+	data->frame->img = mlx_new_image(data->frame->mlx, data->frame->img_width, \
+		data->frame->img_height);
+	data->frame->addr = mlx_get_data_addr(data->frame->img, &data->frame->bits_per_pixel, \
+		&data->frame->line_length, &data->frame->endian);
+	drawcircle_f(data->frame, data->circle->xm, data->circle->ym, data->circle->radius, \
+		data->circle->color);
+	mlx_put_image_to_window(data->frame->mlx, data->frame->win, data->frame->img, 0, 0);
+	mlx_destroy_image(data->frame->mlx, data->frame->img);
 	return (0);
 }
 
-int		main(void)
+static int		init_data(t_data *data)
 {
-	t_game_state	game;
+	if (!(data->frame = (t_frame *)malloc(sizeof(t_frame))))
+		return (1);
+	if (!(data->circle = (t_circle *)malloc(sizeof(t_circle))))
+		return (1);
+	data->frame->mlx = mlx_init();
+	data->frame->win_width = 512;
+	data->frame->win_height = 512;
+	data->frame->win = mlx_new_window(data->frame->mlx, data->frame->win_width, \
+		data->frame->win_height, "cub3D");
+	data->frame->img_width = data->frame->win_width;
+	data->frame->img_height = data->frame->img_height;
+	data->circle->xm = data->frame->img_width / 2;
+	data->circle->ym = data->frame->img_height / 2;
+	data->circle->radius = (data->frame->img_width < data->frame->img_height) ? \
+		data->frame->img_width / 10 : data->frame->img_height / 10;
+	data->circle->color = LIME;
+	return (0);
+}
 
-	game.frame.mlx = mlx_init();
-	game.frame.win_width = 512;
-	game.frame.win_height = 512;
-	game.frame.win = mlx_new_window(game.frame.mlx, game.frame.win_width, \
-		game.frame.win_height, "cub3D");
-	game.frame.img_width = game.frame.win_width;
-	game.frame.img_height = game.frame.win_height;
-	game.frame.color = RED;
-	game.circle.x_pos = game.frame.img_width / 2;
-	game.circle.y_pos = game.frame.img_height / 2;
-	if (game.frame.img_width < game.frame.img_height)
-		game.circle.radius = game.frame.img_width / 10;
-	else
-		game.circle.radius = game.frame.img_height / 10;
-	game.circle.color = LIME;
-	mlx_loop_hook(game.frame.mlx, render_next_frame, &game);
-	mlx_hook (game.frame.win, KeyPress, KeyPressMask, keypress_handler, &game);
-	mlx_loop(game.frame.mlx);
+int				main(void)
+{
+	t_data		data;
+
+	if (init_data(&data))
+		return (1);
+	mlx_loop_hook(data.frame.mlx, render_next_frame, &data);
+	mlx_hook (data.frame.win, KeyPress, KeyPressMask, keypress_handler, &data);
+	mlx_loop(data.frame.mlx);
 	return (0);
 }
